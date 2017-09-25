28a491be11f3a1e02c570892bf9cd072f3de7179
kebeeyong <kebe_4real@yahoo.com>
2017-09-26 00:02:14 +0100

    Going to Heroku ta da

--- dist/server/config/env/production.js
+++ dist/server/config/env/production.js
@@ -4,7 +4,7 @@ var config = {
     //  db : 'mongodb://user:123123123@ds161487.mlab.com:61487/voting-app26',
     //  sessionSecret: process.env.SESSION_SECRET || 'prodsecretkey'
     // db: 'mongodb://user:123123123@ds161487.mlab.com:61487/voting-app26',
-    db: 'mongodb://<dbuser>:<dbpassword>@ds149124.mlab.com:49124/eketvoting-app',
+    db: 'mongodb://kebe.eyong:Lakers23@ds149124.mlab.com:49124/eketvoting-app',
     // sessionSecret: process.env.SESSION_SECRET || 'prodsecretkey'
     sessionSecret: process.env.SESSION_SECRET || 'Lakers23'
 };
