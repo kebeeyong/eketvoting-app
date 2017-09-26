585ccb430aaa72b059bdfab7ec9a6d33b361a905
kebeeyong <kebe_4real@yahoo.com>
2017-09-26 09:36:55 +0100

    Going to Heroku testing something

--- dist/server/config/env/production.js
+++ dist/server/config/env/production.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var config = {
-    //  db : 'mongodb://user:123123123@ds161487.mlab.com:61487/voting-app26',
-    //  sessionSecret: process.env.SESSION_SECRET || 'prodsecretkey'
+      db : 'mongodb://user:123123123@ds161487.mlab.com:61487/voting-app26',
+      sessionSecret: process.env.SESSION_SECRET || 'prodsecretkey'
     
-    db: 'mongodb://kebe.eyong:Lakers23@ds149124.mlab.com:49124/eketvoting-app',
+    //db: 'mongodb://kebe.eyong:Lakers23@ds149124.mlab.com:49124/eketvoting-app',
    //  sessionSecret: process.env.SESSION_SECRET || 'prodsecretkey'
-    sessionSecret: process.env.SESSION_SECRET || 'Lakers23'
+   // sessionSecret: process.env.SESSION_SECRET || 'Lakers23'
 };
 exports.default = config;
 //# sourceMappingURL=production.js.map
