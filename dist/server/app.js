d4f7788b3560639656f23c56c957f54063c4ad03
kebeeyong <kebe_4real@yahoo.com>
2017-09-26 07:20:52 +0100

    Going to Heroku naw

--- dist/server/app.js
+++ dist/server/app.js
@@ -15,8 +15,7 @@ var app = express();
 exports.app = app;
 var MongoStore = connectMongo(session);
 app.set('port', (process.env.PORT || 3000));
-//app.use('/', express.static(path.join(__dirname, '../public')));
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
@@ -36,18 +35,10 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
     console.log('Connected to MongoDB');
     routes_1.default(app);
-   // app.get('/*', function (req, res) {
-    //    res.sendFile(path.join(__dirname, '../public/index.html'));
-    //});
-if (app.get('env') === 'production') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+    app.get('/*', function (req, res) {
+   //     res.sendFile(path.join(__dirname, '../public/index.html'));
+   res.sendFile(path.join(__dirname, '../index.html'));
     });
-}
     if (!module.parent) {
         app.listen(app.get('port'), function () {
             console.log('Server listening on port ' + app.get('port'));
