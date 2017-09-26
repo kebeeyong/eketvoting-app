b54b865b37bc418c02c30b1c46f5f3906c76ceda
kebeeyong <kebe_4real@yahoo.com>
2017-09-26 12:31:41 +0100

    Going to Heroku testing nepeippou

--- server/config/env/production.ts
+++ server/config/env/production.ts
@@ -3,8 +3,8 @@ const config = {
 //  sessionSecret: process.env.SESSION_SECRET || 'prodsecretkey'
   // db: 'mongodb://user:123123123@ds161487.mlab.com:61487/voting-app26',
   	 db: 'mongodb://kebe.eyong:Lakers23@ds149124.mlab.com:49124/eketvoting-app',
-   // sessionSecret: process.env.SESSION_SECRET || 'prodsecretkey'
-   sessionSecret: process.env.SESSION_SECRET || 'Lakers23'
+    sessionSecret: process.env.SESSION_SECRET || 'prodsecretkey'
+ //  sessionSecret: process.env.SESSION_SECRET || 'Lakers23'
 };
 
 
