f7fa3fe3c724196886dfbdff8d884184b8adf2dc
kebeeyong <kebe_4real@yahoo.com>
2017-09-26 07:52:24 +0100

    Going to Heroku okpyopujpph

--- dist/server/config/env/production.js
+++ dist/server/config/env/production.js
@@ -5,8 +5,8 @@ var config = {
     //  sessionSecret: process.env.SESSION_SECRET || 'prodsecretkey'
     // db: 'mongodb://user:123123123@ds161487.mlab.com:61487/voting-app26',
     db: 'mongodb://kebe.eyong:Lakers23@ds149124.mlab.com:49124/eketvoting-app',
-    // sessionSecret: process.env.SESSION_SECRET || 'prodsecretkey'
-    sessionSecret: process.env.SESSION_SECRET || 'Lkers23'
+     sessionSecret: process.env.SESSION_SECRET || 'prodsecretkey'
+    //sessionSecret: process.env.SESSION_SECRET || 'Lakers23'
 };
 exports.default = config;
 //# sourceMappingURL=production.js.map
