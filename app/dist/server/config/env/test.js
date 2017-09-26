9630be73c1d9334dcbb383c8b6f82021e1b3a6c1
kebeeyong <kebe_4real@yahoo.com>
2017-09-26 01:18:41 +0100

    Going to Heroku pe

--- app/dist/server/config/env/test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var config = {
-    db: 'mongodb://localhost:27017/voting-app-test',
-    sessionSecret: process.env.SESSION_SECRET || 'testsecretkey'
-};
-exports.default = config;
-//# sourceMappingURL=test.js.map
