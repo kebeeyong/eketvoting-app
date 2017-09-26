342f2ebbc68754e842c3e3f58675807b155388ec
kebeeyong <kebe_4real@yahoo.com>
2017-09-26 08:21:16 +0100

    Going to Heroku okpyopujpphoslssp

--- dist/server/app.js
+++ dist/server/app.js
@@ -1,42 +1,30 @@
 import * as bodyParser from 'body-parser';
+import * as dotenv from 'dotenv';
 import * as express from 'express';
 import * as morgan from 'morgan';
 import * as mongoose from 'mongoose';
 import * as path from 'path';
-import * as session from 'express-session';
-import * as passport from 'passport';
-import * as connectMongo from 'connect-mongo';
 
-import config from './config/config';
-import setPassport from './config/passport';
 import setRoutes from './routes';
 
 const app = express();
-const MongoStore = connectMongo(session);
-
+dotenv.load({ path: '.env' });
 app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(path.join(__dirname, '../public')));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(morgan('dev'));
 
-mongoose.connect(config.db);
+if (process.env.NODE_ENV === 'test') {
+  mongoose.connect(process.env.MONGODB_TEST_URI);
+} else {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+
 const db = mongoose.connection;
 (<any>mongoose).Promise = global.Promise;
-
-app.use(session({
-  secret: config.sessionSecret,
-  resave: true,
-  saveUninitialized: true,
-  store: new MongoStore({mongooseConnection: db})
-}));
-
-setPassport();
-app.use(passport.initialize());
-app.use(passport.session());
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
@@ -50,7 +38,7 @@ db.once('open', () => {
 
   if (!module.parent) {
     app.listen(app.get('port'), () => {
-      console.log('Server listening on port ' + app.get('port'));
+      console.log('Angular Full Stack listening on port ' + app.get('port'));
     });
   }
 
